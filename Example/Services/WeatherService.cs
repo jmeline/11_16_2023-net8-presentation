141df9e7230433a5c2abf695af2a9ef5c385d8b6
Jacob Meline <jacob.meline@drivetime.com>
2023-11-15 23:38:56 -0700

    solution

--- Example/Services/WeatherService.cs
+++ Example/Services/WeatherService.cs
@@ -8,21 +8,12 @@ public interface IWeatherService
    Task<Root?> GetWeatherForecastByCityAsync(string city); 
 }
 
-public class WeatherService : IWeatherService
+public class WeatherService(HttpClient client, IConfiguration configuration) : IWeatherService
 {
-   private readonly HttpClient _client;
-   private readonly IConfiguration _configuration;
-   
-   public WeatherService(HttpClient client, IConfiguration configuration)
-   {
-      _client = client;
-      _configuration = configuration;
-   }
-
    public async Task<Root?> GetWeatherForecastByCityAsync(string city)
    {
-      var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={_configuration["ApiKey"]}";
-      var response = await _client.GetAsync(url);
+      var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={configuration["ApiKey"]}";
+      var response = await client.GetAsync(url);
       response.EnsureSuccessStatusCode();
       var stringResponse = await response.Content.ReadAsStringAsync();
       return JsonSerializer.Deserialize<Root>(stringResponse);
